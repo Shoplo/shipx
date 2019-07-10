ec405f4feaa343baa4687fc944addceba1d720e5
adrianadamiec <adrianadamiec@gmail.com>
2019-07-10 16:10:30 +0200

    update example

--- example.php
+++ example.php
@@ -5,7 +5,8 @@ require_once __DIR__.'/autoload.php';
 ini_set('display_errors', 'TRUE');
 error_reporting(E_ALL);
 
-$accessToken = 'xxx';
+
+$accessToken = 'XXX';
 
 $guzzleAdapter = new \Shoplo\ShipX\Guzzle\GuzzleAdapter(
     new \GuzzleHttp\Client(['base_uri' => \Shoplo\ShipX\ShipXClient::API_SANDBOX_BASE_URI])
@@ -19,10 +20,12 @@ $client = new \Shoplo\ShipX\ShipXClient($guzzleAdapter, \JMS\Serializer\Serializ
 //print_r($rsp);exit;
 
 $address = new \Shoplo\ShipX\Model\Shipment\Request\AddressRequest();
-$address->line1 = 'Do Przystani 123';
+//$address->line1 = 'Do Przystani 123';
 $address->city = 'Kraków';
 $address->countryCode = 'PL';
 $address->postCode = '30-255';
+$address->street = 'ulica';
+$address->buildingNumber = '123';
 
 $receiver = new \Shoplo\ShipX\Model\Shipment\Request\ReceiverRequest();
 $receiver->firstName = 'Adrian';
@@ -40,40 +43,73 @@ $dimensions->unit = 'mm';
 $weight = new \Shoplo\ShipX\Model\Shipment\Request\WeightRequest();
 $weight->amount = '1';
 
-$customAttr = new \Shoplo\ShipX\Model\Shipment\Request\CustomAttributesRequest();
-$customAttr->targetPoint = 'KRA010';
+//$customAttr = new \Shoplo\ShipX\Model\Shipment\Request\CustomAttributesRequest();
+//$customAttr->targetPoint = 'KRA010';
 
 $parcel = new \Shoplo\ShipX\Model\Shipment\Request\ParcelRequest();
 $parcel->dimensions = $dimensions;
 $parcel->weight = $weight;
 
+
 $shipment = new \Shoplo\ShipX\Model\Shipment\Request\ShipmentRequest();
 $shipment->comments = 'lorem ipsum';
 $shipment->receiver = $receiver;
 $shipment->parcels[] = $parcel;
-$shipment->customAttributes = $customAttr;
-$shipment->service = 'inpost_locker_standard';
+$shipment->isReturn = true;
+//$shipment->customAttributes = $customAttr;
+$shipment->service = 'inpost_courier_standard';//inpost_courier_standard | inpost_locker_standard
+$shipment->onlyChoiceOfOffer = false;
 
-$client->organizationId = '';
-//$shipmentResource = new \Shoplo\ShipX\Resource\ShipmentResource($client);
-$serviceResource = new \Shoplo\ShipX\Resource\ServiceResource($client);
+$client->organizationId = '111';
+$shipmentResource = new \Shoplo\ShipX\Resource\ShipmentResource($client);
+//$serviceResource = new \Shoplo\ShipX\Resource\ServiceResource($client);
 try {
-
+    /** @var \Shoplo\ShipX\Model\Shipment\Response\ShipmentResponse $rsp */
 //    $rsp = $shipmentResource->createShipment($shipment);
-    $rsp = $serviceResource->getServices();
-    print_r($rsp);exit;
+//    $rsp = $shipmentResource->getShipments(163640);
+//    $rsp = $shipmentResource->buyShipment(162501, 243087);
+//    $rsp = $shipmentResource->cancelShipment(161122);
+//    $rsp = $serviceResource->getServices();
+//    print_r($rsp);exit;
 } catch(\Shoplo\ShipX\Exception\BaseException $e) {
     print_r($e->getResponseParsed());exit;
 }
 
-//$shipmentId = 102920;
+$shipmentId = 163640;
 //$labelRequest = new \Shoplo\ShipX\Model\Shipment\Request\ShipmentLabelRequest();
 //$labelRequest->format = \Shoplo\ShipX\Model\Shipment\Request\ShipmentLabelRequest::SHIPMENT_LABEL_FORMAT_PDF;
 //$labelRequest->shipmentIds[] = $shipmentId;
 //$labelRequest->type = \Shoplo\ShipX\Model\Shipment\Request\ShipmentLabelRequest::SHIPMENT_LABEL_TYPE_NORMAL;
 //$rsp = $shipmentResource->getShipmentLabel($labelRequest);
+//
+//header("Content-type:application/pdf");
+//echo $rsp;exit;
+
+$labelRequest = new \Shoplo\ShipX\Model\Shipment\Request\ShipmentLabelRequest();
+$labelRequest->format = \Shoplo\ShipX\Model\Shipment\Request\ShipmentLabelRequest::SHIPMENT_LABEL_FORMAT_PDF;
+$labelRequest->shipmentIds[] = $shipmentId;
+$labelRequest->type = \Shoplo\ShipX\Model\Shipment\Request\ShipmentLabelRequest::SHIPMENT_LABEL_TYPE_NORMAL;
+$rsp = $shipmentResource->getShipmentReturnLabel($labelRequest);
+
+header("Content-type:application/pdf");
+echo $rsp;exit;
 
 //$dispatchPointResource = new \Shoplo\ShipX\Resource\DispatchPointResource($client);
-//$rsp = $dispatchPointResource->getDispatchPoints();
+$dispatchOrder = new \Shoplo\ShipX\Resource\DispatchOrderResource($client);
+//$createDispatchOrderRequest = new \Shoplo\ShipX\Model\DispatchOrder\DispatchOrderRequest();
+//$createDispatchOrderRequest->shipments = [$shipmentId];
+//
+//$addressReq = new \Shoplo\ShipX\Model\DispatchPoint\DispatchOrderAddressRequest();
+//$addressReq->city = 'Kraków';
+//$addressReq->countryCode = 'PL';
+//$addressReq->postCode = '30-255';
+//$addressReq->street = 'ulica';
+//$addressReq->buildingNumber = '123';
+//
+//$createDispatchOrderRequest->address = $addressReq;
+//$rsp = $dispatchOrder->createDispatchOrder($createDispatchOrderRequest);
+$rsp = $dispatchOrder->getDispatchOrder(6010);
+//$rsp = $dispatchOrder->cancelDispatchOrder(6009);
 print_r($rsp);exit;
-exit;
+//6009
+//exit;
