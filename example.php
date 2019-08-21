a297cbbc4f2879157a020b070478df9612117ada
Piotr Antosik <mail@piotrantosik.com>
2019-08-21 09:59:51 +0200

    v1.1.0

--- example.php
+++ example.php
@@ -1,6 +1,8 @@
 <?php
 //echo phpinfo();exit;
-require_once __DIR__.'/autoload.php';
+require_once __DIR__.'/vendor/autoload.php';
+
+Doctrine\Common\Annotations\AnnotationRegistry::registerLoader('class_exists');
 
 ini_set('display_errors', 'TRUE');
 error_reporting(E_ALL);
