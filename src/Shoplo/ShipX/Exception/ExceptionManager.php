8aeb0fa9600dba1f6f4a307552810d674ce70319
Jacek Bednarek <jacek.bednarek1991@gmail.com>
2020-07-27 12:12:45 +0200

    Improve not json response logs

--- src/Shoplo/ShipX/Exception/ExceptionManager.php
+++ src/Shoplo/ShipX/Exception/ExceptionManager.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: adrianadamiec
- * Date: 12.05.2017
- * Time: 15:19
- */
 
 namespace Shoplo\ShipX\Exception;
 
@@ -13,14 +7,21 @@ class ExceptionManager
     public static function throwException(\Throwable $e)
     {
         $body = null;
+
         if (method_exists($e, 'getResponse')) {
-            $body = json_decode($e->getResponse()->getBody()->getContents(), true);
+            $headers = $e->getResponse()->getHeaders();
+            $contentType = $headers['content-type'][0] ?? 'application/json';
+            if ($contentType === 'application/json') {
+                $body = \json_decode($e->getResponse()->getBody()->getContents(), true);
+            }
         }
+
         switch ($e->getCode()) {
             case 400:
                 throw new ValidationException($e, $body);
                 break;
             case 401:
+            case 403:
                 throw new AuthorizationException($e, $body);
                 break;
             case 404:
